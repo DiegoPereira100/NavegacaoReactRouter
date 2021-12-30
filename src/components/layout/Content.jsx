1df911d3b81477c76bfeb44906f11bc05314fe26
diego <diegopgcds10@gmail.com>
2021-12-30 10:41:20 -0300

    Estrutura do Projeto

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Content.css';
+
+const Content = props => (
+
+    <aside className="Content">
+        Componente Content
+    </aside>
+
+)
+
+export default Content
