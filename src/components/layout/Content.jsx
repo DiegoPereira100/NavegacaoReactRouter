483cc7f55cf5dab3003bb36a69d4004b5606124e
diego <diegopgcds10@gmail.com>
2021-12-30 12:18:38 -0300

    Exemplo Página não Encontrada - Projeto Concluido

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import About  from "../../views/examples/About";
 import Param from "../../views/examples/Param";
 import Home from "../../views/examples/Home";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = props => (
 
@@ -13,6 +14,7 @@ const Content = props => (
             <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/param/:id" element={<Param />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     </main>
 
