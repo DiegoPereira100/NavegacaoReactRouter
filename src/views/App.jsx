34000a84817b35c35a40e96a9092a881a7651eb4
diego <diegopgcds10@gmail.com>
2021-12-30 11:46:26 -0300

    Navegação com Parâmetro

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Content from "../components/layout/Content";
 import Menu from "../components/layout/Menu";
 import './App.css';
@@ -6,8 +7,12 @@ import './App.css';
 const App = props => (
 
     <div className="App">
-        <Menu></Menu>
-        <Content></Content>
+
+        <Router>
+            <Menu></Menu>
+            <Content></Content>
+        </Router>
+
     </div>
 
 )
